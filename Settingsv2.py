befd15b4946897ec74a323293b2594a644196c80
josephseering <35943718+josephseering@users.noreply.github.com>
2018-10-11 23:33:09 -0400

    Update Settingsv2.py

--- Settingsv2.py
+++ Settingsv2.py
@@ -12,8 +12,10 @@ PASS = ""
 # the account username for whatever account you're using as a bot. Format "username"
 IDENT = ""
 
-# the scraper will create this many files
+# the scraper will create this many files. I mostly just use this because if I put all of the output into a single file it becomes to big to manage, so I separate it into multiple files. 
 ROUNDS = 2
 
 # how long it will scrape for in each file (in seconds)
 ROUNDLENGTH = 28800
+
+# With the numbers I've entered here (in ROUNDS and ROUNDLENGTH), it'll scrape into one file for 28800 seconds then create a second file and scrape into it for 28800 seconds. These obviously can be changed.
