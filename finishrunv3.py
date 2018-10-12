4fc83f39a4b83afd1fb00235fd696639f2cf16a3
josephseering <35943718+josephseering@users.noreply.github.com>
2018-10-11 23:07:12 -0400

    Delete finishrunv3.py

--- finishrunv3.py
+++
@@ -1,7 +0,0 @@
-# If the main run program was stopped prematurely, this runs the sorting and banfinding code
-
-from sorter import sortbyUsername
-from banfinder import MarkBannedMessages
-
-sortbyUsername()
-MarkBannedMessages()
